523b7b406047fa344f0898ac8d33f6a7c6fd845a
Ameth-Toledo <233363@ids.upchiapas.edu.mx>
2024-10-26 17:13:41 -0600

    shared QR

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
+  isModalOpen = false;
+  isAlertVisible = false;
 
+  openModal(){
+    this.isModalOpen = true;
+  }
+
+  closeModal() {
+    this.isModalOpen = false;
+  }
+
+  copyLink() {
+    const link = 'https://ameth-toledo.github.io/prueba/';
+    navigator.clipboard.writeText(link).then(() => {
+      this.showAlert();
+    });
+  }
+
+  showAlert() {
+    this.isAlertVisible = true;
+    setTimeout(() => {
+      this.isAlertVisible = false;
+    }, 3000);
+  }
 }
